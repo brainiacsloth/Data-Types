b873fe5c8d79c10ea3846cdb123c049732bf1861
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-05 09:06:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        string name = "Vumboni";
+        int age = 27;
+
+        bool isTall = false;
+        double weight = 80.2;
+        
+        Console.WriteLine(name + " is " + age + ", he weighs " + weight + "kg.");
+        Console.WriteLine("It is " + isTall + " that he is tall.");
+        
+        Console.ReadLine();
+    }
+}
